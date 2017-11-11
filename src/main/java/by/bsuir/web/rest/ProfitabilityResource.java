f3026b7ba66bbdb2a2556b65604e0400446d3f30
Andrey Mukamolow <fobo66@protonmail.com>
2017-11-11 22:04:14 +0300

    updated REST controller

--- src/main/java/by/bsuir/web/rest/ProfitabilityResource.java
+++ src/main/java/by/bsuir/web/rest/ProfitabilityResource.java
@@ -1,8 +1,15 @@
 package by.bsuir.web.rest;
 
+import by.bsuir.repository.CryptocurrencyRepository;
+import by.bsuir.security.AuthoritiesConstants;
 import by.bsuir.service.ProfitabilityCalculatorService;
+import com.codahale.metrics.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +22,20 @@ public class ProfitabilityResource {
     private final Logger log = LoggerFactory.getLogger(ProfitabilityResource.class);
 
     private final ProfitabilityCalculatorService profitabilityCalculatorService;
+    private final CryptocurrencyRepository cryptocurrencyRepository;
 
-    public ProfitabilityResource(ProfitabilityCalculatorService profitabilityCalculatorService) {
+    public ProfitabilityResource(ProfitabilityCalculatorService profitabilityCalculatorService,
+                                 CryptocurrencyRepository cryptocurrencyRepository) {
         this.profitabilityCalculatorService = profitabilityCalculatorService;
+        this.cryptocurrencyRepository = cryptocurrencyRepository;
+    }
+
+    @GetMapping("/profitability")
+    @Timed
+    @Secured(AuthoritiesConstants.USER)
+    public ResponseEntity<Boolean> calculateProfitability() {
+        log.debug("Calculating profitability");
+        return new ResponseEntity<>(
+            profitabilityCalculatorService.isMiningProfitable(cryptocurrencyRepository.getOne(1L)), HttpStatus.OK);
     }
 }
